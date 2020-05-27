2583442e6a0e5e9819bff86425f71fcf2f0b51ce
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2020-05-27 17:08:47 +0300

    NodeJS bindings: adding asset allocation info for issuance example

--- ffi/nodejs/example.js
+++ ffi/nodejs/example.js
@@ -1,10 +1,10 @@
 const ex = require('./build/Release/rgb_node');
 
 const config = {
-    network: "bitcoin",
-    stash_endpoint: "ipc:{data_dir}/{network}/stashd.rpc",
+    network: "testnet",
+    stash_endpoint: "ipc:/home/orlovsky/repo/rgb-node/data/testnet/stashd.rpc",
     contract_endpoints: {
-        Fungible: "ipc:{data_dir}/{network}/fungibled.rpc"
+        Fungible: "ipc:/home/orlovsky/repo/rgb-node/data/testnet/fungibled.rpc"
     }
 };
 
@@ -14,6 +14,7 @@ ex.start_rgb(JSON.stringify(config))
         ticker: "USDT",
         name: "USD Tether",
         issue_structure: "SingleIssue",
+        allocations: [{ coins: 100, vout:0, txid: "0313ba7cfcaa66029a1a63918ebc426259f00953016c461663315d1bf6b83ab4" }],
         precision: 0,
     })))
     .catch(e => console.log(e));
