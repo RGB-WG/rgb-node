e5f063067740d0432347ef74126190e80b8cb699
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-12-19 17:49:48 +0100

    Fix docrs builds and code style requirements

--- rpc/src/lib.rs
+++ rpc/src/lib.rs
@@ -8,7 +8,17 @@
 // You should have received a copy of the MIT License along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
-#![recursion_limit = "256"]
+// Coding conventions
+#![deny(
+    non_upper_case_globals,
+    non_camel_case_types,
+    non_snake_case,
+    unused_mut,
+    unused_imports,
+    dead_code,
+    //missing_docs
+)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
 #[macro_use]
 extern crate amplify;
